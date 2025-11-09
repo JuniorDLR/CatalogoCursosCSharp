3525532eb2e87b5082d269a98a8f2bf83640f464
JuniorDLR <juniordelarocha15@hotmail.com>
2025-11-09 13:00:11 -0600

    Estructura del curso  y el metodo main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Curso
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}. {Nombre} - {Descripcion}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Junaxer estuvo aqui!");
+    }
+
+}
